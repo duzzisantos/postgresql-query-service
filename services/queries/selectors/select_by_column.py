7b30986e7e20ad64ad9b1ccaae09c4b8a5193d93
Duzie <user@MacBook-Pro.local>
2025-05-03 17:38:52 +0200

    added selectors for multiple columns

--- services/queries/selectors/select_by_column.py
+++ services/queries/selectors/select_by_column.py
@@ -1,5 +1,13 @@
 from query_builders.selectors import select_queries
 
 query = select_queries.SelectQueries()
-class SelectByColumn():
-   
+
+class SelectByColumn:
+    def getByColumns(table: str, columns: list[str]):
+        return query.selectByColumns(table, columns)
+    
+    def getByColumnsAndOrderBy(table: str, columns: list[str], order: str):
+        return query.selectByColumnsAndOrderBy(table, columns, order)
+    
+    def getByColumnsAndLimit(table: str, columns: list[str], limit: int):
+        return query.selectByColumnsAndLimit(table, columns, limit)
