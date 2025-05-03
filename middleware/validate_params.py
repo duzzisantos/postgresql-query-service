0c8c053d62c4858932055269fb3b50ec081c0123
Duzie <user@MacBook-Pro.local>
2025-05-03 18:03:27 +0200

    added parameter validation

---
+++ middleware/validate_params.py
@@ -0,0 +1,9 @@
+def validateParams(params: str | int | list[str]):
+    if (type(params).__eq__(str) and params.__eq__("")):
+        return {"message": f"{params} returned an empty string", "result": False}
+    elif(type(params).__eq__(list[str]) and len(params).__eq__(0)):
+         return {"message": f"{params} returned an empty array of strings", "result": False}
+    elif(type(params).__eq__(int) and not params):
+         return {"message": f"{params} returned no integer", "result": False}
+    else:
+         return {"message": f"{params} successfully validated", "result": True}
