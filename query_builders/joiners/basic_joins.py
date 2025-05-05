c5de742e083e115b4b322110a7cdfd80e2dfba16
Duzie <user@MacBook-Pro.local>
2025-05-05 21:59:37 +0200

    Refactored BasicJoin class - added constructor, made joiner reusable

--- query_builders/joiners/basic_joins.py
+++ query_builders/joiners/basic_joins.py
@@ -1,14 +1,30 @@
 from joiners.joiner_util import use_joiner
 
-class BasicJoin:
-    def innerJoin(columns: list[str], join_type: str, primary_table: str, secondary_table: str, common_key: str, cursor: function):
-        return use_joiner(columns, join_type, primary_table, secondary_table, common_key, cursor)
+class BasicJoin():
+    def __init__(self, columns: list[str], join_type: str, primary_table, secondary_table, common_key: str, cursor: function):
+        self.columns = columns
+        self.join_type = join_type
+        self.primary_table = primary_table
+        self.secondary_table = secondary_table
+        self.common_key = common_key
+        self.cursor = cursor
+        self.use_joiner = use_joiner(self.columns, self.join_type, self.primary_table, self.secondary_table, self.common_key, self.cursor)
+
     
-    def leftJoin(columns: list[str], join_type: str, primary_table: str, secondary_table: str, common_key: str, cursor: function):
-        return use_joiner(columns, join_type, primary_table, secondary_table, common_key, cursor)
-    
-    def rightJoin(columns: list[str], join_type: str, primary_table: str, secondary_table: str, common_key: str, cursor: function):
-        return use_joiner(columns, join_type, primary_table, secondary_table, common_key, cursor)
-    
-    def fullJoin(columns: list[str], join_type: str, primary_table: str, secondary_table: str, common_key: str, cursor: function):
-        return use_joiner(columns, join_type, primary_table, secondary_table, common_key, cursor)
+
+    def innerJoin(self) -> function:
+            return self.use_joiner
+        
+    def leftJoin(self) -> function:
+            return self.use_joiner
+        
+    def rightJoin(self) -> function:
+            return self.use_joiner
+        
+    def fullJoin(self) -> function:
+            return self.use_joiner
+
+
+        
+        
+
