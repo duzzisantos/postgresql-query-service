44a28530c87d36713d291e6c1dc77f159985432c
Duzie <user@MacBook-Pro.local>
2025-05-05 20:19:45 +0200

    Added creators class and methods

---
+++ query_builders/mutators/creators.py
@@ -0,0 +1,11 @@
+class Creators:
+    def createOne(table: str, columns: list[str], values: list[str], cursor: function):
+        multi_cols = ", ".join(columns)
+        vals = ", ".join(values)
+        return cursor.execute("INSERT INTO %s (%s) VALUES (%s)", (table, multi_cols, vals))
+    
+
+    def createMany(table: str, columns: list[str], values: list[tuple], cursor: function):
+        multi_cols = ", ".join(columns)
+        multi_vals = ", ".join(values)
+        return cursor.execute("INSERT INTO %s (%s) VALUES (%s)", (table, multi_cols, multi_vals))
